3e1a4d31a8920066399db5ec6f2618baaefe430a
Farras Rafi Permana <farras.rafi@ui.ac.id>
2023-10-29 09:53:28 +0700

    Backend Initialize and Local Authentication

---
+++ Backend/src/db.js
@@ -0,0 +1,36 @@
+// for .env file
+require("dotenv").config();
+
+const { Client } = require("pg");
+
+// connect to database using pg
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+const URL = `postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?options=project%3D${ENDPOINT_ID}`;
+
+// const db = new Client({
+//   connectionString: URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+//   idleTimeoutMillis: 0,
+//   connectionTimeoutMillis: 0,
+// });
+
+//connect db from localhost
+const db = new Client({
+  host:"localhost",
+  user:"postgres",
+  database:"calterra",
+  port:5432,
+});
+
+db.connect((err) => {
+  try {
+    console.log("Connected to DB!");
+  } catch (error) {
+    console.log(err);
+    return;
+  } 
+});
+
+module.exports = db;
